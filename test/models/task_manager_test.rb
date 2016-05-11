c27bbdc5a5acf42a94ce70ad40c8cd4840dbe82b
Charles Kaminer <ckamine@g.clemson.edu>
2016-05-11 11:36:40 -0600

    reconfigured, added test files

---
+++ test/models/task_manager_test.rb
@@ -0,0 +1,16 @@
+require_relative '../test_helper'
+
+class TaskManagerTest < Minitest::Test
+  include TestHelpers
+
+  def test_it_creates_a_task
+    task_manager.create({title: "TDD", description: "Learn to Test"})
+
+    task = task_manager.find(1)
+
+    assert_equal "TDD", task.title
+    assert_equal "Learn to Test", task.description
+    assert_equal 1, task.id
+  end
+
+end
